cda24f081aea9b7a921d9b0935d9d0e5b20b3640
Tim Knudsen <TimTheSmartCoder@outlook.dk>
2016-11-08 11:59:10 +0100

    Added so that you can ask the factory to return a specific interface.

--- ServiceGateway/ServiceGatewayManager.cs
+++ ServiceGateway/ServiceGatewayManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Entities;
@@ -13,19 +14,36 @@ namespace ServiceGateway
 {
     public class ServiceGatewayManager
     {
+        /// <summary>
+        /// Get the Service Gateway for the given type.
+        /// </summary>
+        /// <typeparam name="T">Entity to get ServiceGateway for.</typeparam>
+        /// <returns></returns>
         public static IServiceGateway<T> GetService<T>() where T : AbstractEntity
+        {
+            return GetService<T, IServiceGateway<T>>();
+        }
+
+        /// <summary>
+        /// Gets the ServiceGateway for the given type and returns the given
+        /// Interface.
+        /// </summary>
+        /// <typeparam name="T">Entity to get ServiceGateway for.</typeparam>
+        /// <typeparam name="TK">Interface type to return.</typeparam>
+        /// <returns></returns>
+        public static TK GetService<T, TK>() where T : AbstractEntity where TK : IServiceGateway<T>
         {
             //Namespace to the Service Gateway classes.
             string @namespace = "ServiceGateway.ServiceGateways";
 
             //Queries all of the Service Gateways classes except the Abstract.
             IEnumerable<Type> classes = from t in Assembly.GetExecutingAssembly().GetTypes()
-                where t.IsClass && t.Namespace == @namespace && !t.IsAbstract
-                select t;
+                                        where t.IsClass && t.Namespace == @namespace && !t.IsAbstract
+                                        select t;
 
             foreach (Type @class in classes)
                 if (@class != null && @class.BaseType.GetGenericArguments()[0].Name == typeof(T).Name)
-                    return (IServiceGateway<T>) Activator.CreateInstance(@class);
+                    return (TK)Activator.CreateInstance(@class);
 
 
             throw new ServiceGatewayException($"Failed to find a gateway for {typeof(T).Name}");
